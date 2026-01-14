f8cdaca6c2f3e2e7bdf5de0a7f3b3c96e6507e1f
joao <joaoteodorico14@gmail.com>
2026-01-14 16:59:28 -0300

    Create Second_Lesson.py
    Examples of string literals, escape sequences, and raw strings in Python.

---
+++ Python/Second_Lesson.py
@@ -0,0 +1,15 @@
+'''
+Python = Programming Language
+Typing discipline : Dynamically typed (Strongly typed)
+Strings is text inside a single quote(' ') or double quote(" ")
+'''
+
+print("Hello, World!")
+print('Hello, WOrld!')
+print('Hello, "W0rld!"')
+
+#Escape Sequence
+print("Hello,\'W3rld!\'")
+
+#r
+print(r"Hello,\'WUrld!\'")
