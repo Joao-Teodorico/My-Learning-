3da3dd94bd2befde988818b54ef826574f9eddc0
joao <joaoteodorico14@gmail.com>
2026-01-14 11:48:51 -0300

    Create the first lesson
    Basic Python examples demonstrating comments, docstrings, and the print() function with sep and end.

---
+++ Python/First_Lesson.py
@@ -0,0 +1,15 @@
+"""
+This is a docstring comment that can span multiple lines.
+"""
+
+''' 
+This is a docstring comment that can span multiple lines. 
+'''
+
+# This Allows you to write comments in Python
+print ('Hello, World!') # You can comment a side like this
+#You can comment Below (Not Recommended)
+
+#\n -> LF (Line Feed)
+print (1+1, 56,23,4, sep='_', end='\n\n') ## print is a function that outputs data to the console, regardless of the data type.
+print (1+7, 44,22,sep='_', end='\n\n')
