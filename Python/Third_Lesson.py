07ea5c832fd77e3be793fd7d4e92f74f221673fc
João Teodorico <joaoteodorico14@gmail.com>
2026-01-27 12:07:48 -0300

    Update Third_Lesson.py
    Introduce a lesson covering numbers, int and float types, and the type() function in Python

---
+++ Python/Third_Lesson.py
@@ -0,0 +1,20 @@
+# Types of int and float
+
+# int – Integer numbers
+# int is used to represent whole numbers, without decimal points.
+# It can represent both positive and negative values.
+print (11)
+print (-11)
+print (0)
+
+# float – Floating-point numbers
+# float is used to represent numbers that contain decimal points.
+# It can represent both positive and negative values with fractional parts.
+
+print (11.1,9.5)
+print (5.4,8.2)
+
+# The type()function shaws the data type of a value in Python  
+print ( type ('Joao') )
+print ( type (32) )
+print ( type (1.1) )
